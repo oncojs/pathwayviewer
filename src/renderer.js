36fc0054e9778da091319e45b1faa76450cd2396
Joel <joel.weiser@oicr.on.ca>
2017-01-10 18:04:36 -0500

    removed console.log statement

--- src/renderer.js
+++ src/renderer.js
@@ -36,7 +36,6 @@ export class Renderer {
     this.svg = svg;
     this.config = _.defaultsDeep({}, config, defaultConfig);
     this.colors = this.config.colors;
-    console.log(this.config);
     this.urlPath = this.config.urlPath;
     
     this.modelObjectToSVG = new WeakMap();
